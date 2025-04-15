2471b5c2480169a1edcdae914f8c17c4a8b8a044
Lowend <161211037+unknight@users.noreply.github.com>
2025-04-16 04:22:57 +0700

    update navbar

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.jpg"
 import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Navbar() {
     return <header className="shadow-sm">
-        <div className="max-w-7xl mx-auto p-3 flex items-center justify-between gap-3">
+    <div className="max-w-7xl mx-auto p-3 flex items-center justify-between gap-3">
             <Link href="/resumes" className="flex items-center gap-2">
                 <Image src={logo} alt="Logo" width={35} height={35} className="rounded-full"/>
                 <span className="text-xl font-bold tracking-tight">Auto Resume</span>
             </Link>
+
+        <div>
+            <ThemeToggle />
             <UserButton
                 appearance={{
                     elements: {
@@ -19,7 +25,8 @@ export default function Navbar() {
                         },
                     },
                 }}
-            />
+            ></UserButton>
         </div>
+    </div>
     </header>
 }
