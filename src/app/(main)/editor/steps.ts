04e0dface3ad080d73bc9b98ed2dd56a9b3d8bf7
Lowend <161211037+unknight@users.noreply.github.com>
2025-04-25 22:28:55 +0700

    add work experience

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,11 +1,14 @@
+import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
 
 export const steps: {
     title: string;
-    component: React.ComponentType;
+    component: React.ComponentType<EditorFormProps>;
     key: string;
 }[] = [
     {title: "Thông tin chung", component: GeneralInfoForm, key: "general-info"},
     {title: "Thông tin cá nhân", component: PersonalInfoForm, key: "personal-info"},
+    {title: "Kinh nghiệm làm việc", component: WorkExperienceForm, key: "work-experience"},
 ]
