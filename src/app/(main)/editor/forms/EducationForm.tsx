1c450285ff27958e36dea35e301548d5f39c2744
Lowend <161211037+unknight@users.noreply.github.com>
2025-04-28 15:20:46 +0700

    add debounce to education form

--- src/app/(main)/editor/forms/EducationForm.tsx
+++ src/app/(main)/editor/forms/EducationForm.tsx
@@ -1,127 +1,239 @@
-import { FormField, FormItem, FormControl, FormLabel, Form } from "@/components/ui/form";
-import { EditorFormProps } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { EditorFormProps } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import { educationSchema, EducationValues } from "@/lib/validation";
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { GripHorizontal } from "lucide-react";
+import { useEffect } from "react";
 import { useFieldArray, useForm, UseFormReturn } from "react-hook-form";
-import { Button } from "@/components/ui/button";
 
-export default function EducationForm({resumeData, setResumeData}: EditorFormProps) {
-    const form = useForm<EducationValues>({
+export default function EducationForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<EducationValues>({
     resolver: zodResolver(educationSchema),
-        defaultValues: {
-            educations: resumeData.educations || []
-        }
-    })
+    defaultValues: {
+      educations: resumeData.educations || [],
+    },
+  });
 
-    const {fields, append, remove} = useFieldArray({
-        control: form.control,
-        name: "educations",
-    })
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...resumeData,
+        educations: values.educations?.filter((edu) => edu !== undefined) || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
 
-    return <div className="max-w-xl mx-auto space-y-6">
-        <div className="space-y-1.5 text-center">
-            <h2 className="text-2xl font-semibold">Kinh nghiệm làm việc</h2>
-            <p className="text-sm text-muted-foreground">
-                Thêm thông tin về học vấn của bạn để nhà tuyển dụng hiểu rõ hơn về bạn.
-            </p>
-        </div>
-        <Form {...form}>
-            <form className="space-y-3">
-                {fields.map((field, index) => (
-                    <EducationItem 
-                    key={field.id}
-                    index={index}
-                    form={form}
-                    remove={remove}
-                    />
-                ))}
-                <div className="flex justify-center">
-                    <Button type="button" onClick={() => append({
-                        degree: "",
-                        school: "",
-                        startDate: "",
-                        endDate: "",
-                    })}>
-                        Thêm kinh nghiệm làm việc
-                    </Button>
-                </div>
-            </form>
-        </Form>
+  const { fields, append, remove, move } = useFieldArray({
+    control: form.control,
+    name: "educations",
+  });
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      const oldIndex = fields.findIndex((field) => field.id === active.id);
+      const newIndex = fields.findIndex((field) => field.id === over.id);
+      move(oldIndex, newIndex);
+      return arrayMove(fields, oldIndex, newIndex);
+    }
+  }
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Học vấn</h2>
+        <p className="text-sm text-muted-foreground">
+          Thêm vào các thông tin về học vấn của bạn
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+            modifiers={[restrictToVerticalAxis]}
+          >
+            <SortableContext
+              items={fields}
+              strategy={verticalListSortingStrategy}
+            >
+              {fields.map((field, index) => (
+                <EducationItem
+                  id={field.id}
+                  key={field.id}
+                  index={index}
+                  form={form}
+                  remove={remove}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
+          <div className="flex justify-center">
+            <Button
+              type="button"
+              onClick={() =>
+                append({
+                  degree: "",
+                  school: "",
+                  startDate: "",
+                  endDate: "",
+                })
+              }
+            >
+              Thêm thông tin học vấn
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
+  );
 }
 
-interface EducaionItemProps {
-    form: UseFormReturn<EducationValues>;
-    index: number;
-    remove: (index: number) => void;
+interface EducationItemProps {
+  id: string;
+  form: UseFormReturn<EducationValues>;
+  index: number;
+  remove: (index: number) => void;
 }
 
-function EducationItem({form, index, remove}: EducaionItemProps) {
-    return <div className="space-y-3 border rounded-md bg-background">
-        <div className="flex justify-between gap-2">
-            <span className="font-semibold">Học vấn {index + 1}</span>
-            <GripHorizontal className="size-5 cursor-grab text-muted-foreground" />
-        </div>
-        <FormField
+function EducationItem({ id, form, index, remove }: EducationItemProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
+
+  return (
+    <div
+      className={cn(
+        "space-y-3 rounded-md border bg-background p-3",
+        isDragging && "relative z-50 cursor-grab shadow-xl",
+      )}
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+      }}
+    >
+      <div className="flex justify-between gap-2">
+        <span className="font-semibold">Học vấn {index + 1}</span>
+        <GripHorizontal
+          className="size-5 cursor-grab text-muted-foreground focus:outline-none"
+          {...attributes}
+          {...listeners}
+        />
+      </div>
+      <FormField
         control={form.control}
         name={`educations.${index}.degree`}
-        render={({field}) => (
-            <FormItem>
-                <FormLabel>Bằng cấp</FormLabel>
-                <FormControl>
-                    <Input {...field} autoFocus />
-                </FormControl>
-            </FormItem>
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Bằng cấp</FormLabel>
+            <FormControl>
+              <Input {...field} autoFocus />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
         )}
-        />
-        <FormField
+      />
+      <FormField
         control={form.control}
         name={`educations.${index}.school`}
-        render={({field}) => (
-            <FormItem>
-                <FormLabel>Trường học</FormLabel>
-                <FormControl>
-                    <Input {...field} />
-                </FormControl>
-            </FormItem>
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Trường</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
         )}
+      />
+      <div className="grid grid-cols-2 gap-3">
+        <FormField
+          control={form.control}
+          name={`educations.${index}.startDate`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ngày bắt đầu</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="date"
+                  value={field.value?.slice(0, 10)}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        <div className="grid grid-cols-2 gap-3">
-        <FormField 
-            control={form.control}
-            name={`educations.${index}.startDate`}
-            render={({field}) => (
-                <FormItem>
-                    <FormLabel>Ngày bắt đầu</FormLabel>
-                    <FormControl>
-                        <Input {...field}
-                        type="date"
-                        value={field.value?.slice(0, 10)}
-                        />
-                    </FormControl>
-                </FormItem>
-            )}
-            />
-            <FormField 
-            control={form.control}
-            name={`educations.${index}.endDate`}
-            render={({field}) => (
-                <FormItem>
-                    <FormLabel>Ngày kết thúc</FormLabel>
-                    <FormControl>
-                        <Input {...field}
-                        type="date"
-                        value={field.value?.slice(0, 10)}
-                        />
-                    </FormControl>
-                </FormItem>
-            )}
-            />
-        </div>
-        <Button type="button" variant="destructive" onClick={() => remove(index)} className="w-full">
-            Xóa
-        </Button>
+        <FormField
+          control={form.control}
+          name={`educations.${index}.endDate`}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ngày kết thúc</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  type="date"
+                  value={field.value?.slice(0, 10)}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <Button variant="destructive" type="button" onClick={() => remove(index)}>
+        Xóa
+      </Button>
     </div>
+  );
 }
