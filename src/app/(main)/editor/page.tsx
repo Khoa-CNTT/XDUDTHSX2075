fe9a4e45b6895e17bd8d5e42cf56c7f7aaa1d104
Lowend <161211037+unknight@users.noreply.github.com>
2025-05-06 01:23:49 +0700

    update resume editor main page

--- src/app/(main)/editor/page.tsx
+++ src/app/(main)/editor/page.tsx
@@ -1,11 +1,32 @@
+import prisma from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 import ResumeEditor from "./ResumeEditor";
 
-export const metadata: Metadata = {
-    title: "Trang chỉnh sửa hồ sơ lý lịch",
-    description: "CV Editor page",
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>;
 }
 
-export default function Page() {
-    return <ResumeEditor />
+export const metadata: Metadata = {
+  title: "Tạo CV cho bạn",
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const { resumeId } = await searchParams;
+
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const resumeToEdit = resumeId
+    ? await prisma.resume.findUnique({
+        where: { id: resumeId, userId },
+        include: resumeDataInclude,
+      })
+    : null;
+
+  return <ResumeEditor resumeToEdit={resumeToEdit} />;
 }
